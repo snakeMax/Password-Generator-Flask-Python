2745862e7ae91e568fa53eb62c8d669dd5a33dcc
snakeMax <38764043+snakeMax@users.noreply.github.com>
2022-09-09 14:15:25 +0200

    Added databases
    the DBs are local on my school computer

--- server.py
+++ server.py
@@ -1,14 +1,37 @@
 from flask import Flask, render_template
 import random, string
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = '2it'
+ 
+mysql = MySQL(app)
+
 
 def randomize():
     return ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits + "!!!!!___", k=11))
 
+def sql_command(statement):
+    cursor = mysql.connection.cursor()
+    cursor.execute(statement)
+    cursor.close()
+    return '<h1>Query success!</h1>'
+
+def connect_to_base():
+    try:
+        return sql_command(''' SELECT 1 ''')
+    except Exception as e:
+        print("\nThe error:\n" + str(e) + "\n")
+        return '<h1>Sql error!</h1>'
+
+
 @app.route('/')
 def website():
-    rstring = randomize()
-    return render_template('index.html', r = rstring)
+    return render_template('index.html', r = randomize(), debug = connect_to_base())
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host='localhost', port=5000)
